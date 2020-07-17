c74dc18010920976aed5f644891cf6d8c82123c3
Tim Moloney <tmoloney@trihydro.com>
2020-07-17 08:35:25 -0600

    edit code

--- processing.R
+++ processing.R
@@ -3,7 +3,8 @@ library(dplyr)
 library(tidyr)
 astm<-read_excel("Remediation_WBS_Dictionary.xlsx",sheet=1)
 astm<-astm[!is.na(astm$`2nd Level`),]
-astm_1stlvl<-astm[astm$`2nd Level`==0,]
+tasks<-c(3,17,34:36,38,43,44,54,58,64,64,77:79,91:93,135,137,144,174:178,182,184)
+astm_1stlvl<-astm[astm$...1 %in% tasks,]
 rates<-read_excel("Chevron 2019-2020 SOC.xlsx")
 
 
@@ -11,10 +12,13 @@ rates<-read_excel("Chevron 2019-2020 SOC.xlsx")
 mdat<-matrix(0,nrow=nrow(astm_1stlvl)+1, ncol=nrow(rates),)
 df<-data.frame(mdat)
 colnames(df)<-rates$Position
-df$category<-c("Rate",astm_1stlvl$`Environmental Management`)  
-df[1,]<-c(rates$Rate,"Rate")
 
-t<-as_tibble(df)
+t<-tibble(df)
+t<-t %>%
+  mutate(across(.cols= everything(), as.numeric))
+t$category<-c("Rate",astm_1stlvl$`Environmental Management`)  
+
+
 t<-t[,c(27,1:26)]
 
 p<-t %>%
